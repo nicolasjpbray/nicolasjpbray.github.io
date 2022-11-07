801b5c591c7bbf4f9e73d2ffd851ab525d3e50e9
nicolasjpbray <njpbray@gmail.com>
2022-11-06 22:19:34 -0500

    Add files via upload

---
+++ nmc/4020/assignments/week--11/assignment--2/assets/js/newVue.js
@@ -0,0 +1,11 @@
+var header = new Vue({
+	el: '#header',
+		data: {
+			yourName: 'Nicolas Bray',
+			description: "I'm a fifth year EMST student at the University of Georgia",
+			linkOne: "https://nicolasjpbray.github.io/nmc/",
+			link2: "https://nicolasjpbray.github.io/",
+			link3: "images/puppy.jpg",
+			link4: ""
+		}
+})
