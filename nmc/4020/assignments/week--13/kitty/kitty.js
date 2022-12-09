626fdade4ba354a62056da43c376717e55eecb94
nicolasjpbray <njpbray@gmail.com>
2022-12-09 12:30:42 -0500

    Add files via upload

---
+++ nmc/4020/assignments/week--13/kitty/kitty.js
@@ -0,0 +1,23 @@
+new Vue({
+    el: '#app',
+    data: {
+        catArray: [],
+        breeds: []
+    },
+    created() {
+        this.loadNextImage();
+    },
+    methods: {
+        async loadNextImage() {
+            try {
+                axios.defaults.headers.common['x-api-key'] = "5707ff43-c0b5-456f-864a-78a03c24ea46"
+                let response = await axios.get('https://api.thecatapi.com/v1/breeds');
+                this.catArray = response.data;
+                
+            } catch (err) {
+                console.log(err)
+            }
+
+        }
+    }
+})
