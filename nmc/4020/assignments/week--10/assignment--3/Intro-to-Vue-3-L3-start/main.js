627868ce5d769408f52fecb3367d144d98447476
nicolasjpbray <njpbray@gmail.com>
2023-05-08 02:42:48 -0400

    Delete nmc directory

--- nmc/4020/assignments/week--10/assignment--3/Intro-to-Vue-3-L3-start/main.js
+++
@@ -1,9 +0,0 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg', 
-            thingUrl: 'https://www.google.com/'
-        }
-    }
-})
